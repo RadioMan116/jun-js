548253dccc4caba641fd910579d31aaf7fb32198
RadioMan116 <git@github.com:RadioMan116>
2018-09-02 19:15:59 +0300

    111

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Studio-512
-Work
+# MIB
--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,82 @@
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const cleanCSS = require("gulp-clean-css");
-const browserSync = require("browser-sync").create();
-const sourcemaps = require("gulp-sourcemaps");
-const gcmq = require("gulp-group-css-media-queries");
-const preproc = require("gulp-less");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const preproc = require('gulp-less');
+const gcmq = require('gulp-group-css-media-queries');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
+const ejs = require('gulp-ejs');
+// var babel = require('gulp-babel');
+const fs = require('fs');
 
-const config = {
-  src: "./src",
-  css: {
-    watch: "/precss/**/*.less",
-    src: "/precss/styles.less",
-    dest: "/style"
-  },
-  html: {
-    src: "/index.html"
-  }
-};
-
-gulp.task("build", function() {
-  gulp
-    .src(config.src + config.css.src)
-    .pipe(sourcemaps.init())
-    .pipe(preproc())
-    .pipe(gcmq())
-    .pipe(
-      autoprefixer({
-        browsers: ["> 0.1%"],
-        cascade: false
-      })
-    )
-    // .pipe(cleanCSS({
-    //     level: 2
-    // }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(config.src + config.css.dest))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
+gulp.task('grid', function () {
+    return gulp.src('src/style/**/grid-system.scss')
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/style/'));
 });
 
-gulp.task("watch", ["browserSync"], function() {
-  gulp.watch(config.src + config.css.watch, ["build"]);
-  gulp.watch(config.src + config.html.src, browserSync.reload);
+gulp.task('sass', function () {
+    return gulp.src('src/style/**/*.scss')
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/style/'));
+});
+gulp.task('preproc', function () {
+    return gulp.src('src/precss/styles.less')
+        .pipe(preproc())
+        .pipe(gcmq())
+        .pipe(autoprefixer({
+            browsers: ['> 0.1%'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(gulp.dest('dist/style/'));
 });
 
-gulp.task("browserSync", function() {
-  browserSync.init({
-    server: {
-      baseDir: config.src
-    }
-  });
+gulp.task('scripts', function () {
+    return gulp.src('src/js/**/*.js')
+        // .pipe(babel({
+        //     presets: ['es2015']
+        // }))
+        .pipe(uglify().on('error', function (e) {
+            console.log(e);
+        }))
+        .pipe(gulp.dest('./dist/js/'))
 });
+
+gulp.task('sections', ['preproc'], function () {
+    var criticalStyle = fs.readFileSync('./dist/style/styles.css', 'utf8');
+    var version = '4';
+    return gulp.src('./src/*.html')
+        // .pipe(ejs({criticalStyle: criticalStyle, version: version}, {}, {ext: '.html'}))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('assets', function () {
+    gulp.src(['./src/img/**/*'])
+        .pipe(gulp.dest('./dist/img'))
+});
+
+gulp.task('fonts', function () {
+    gulp.src(['./src/fonts/**/*'])
+        .pipe(gulp.dest('./dist/fonts'))
+});
+
+gulp.task('watch', ['default'], function () {
+    gulp.watch('src/js/**/*.js', ['scripts']);
+    gulp.watch('src/**/*.ejs', ['sections']);
+    gulp.watch('src/style/**/*.scss', ['sass', 'sections']);
+    gulp.watch('src/style/**/*.less', ['preproc']);
+    gulp.watch('src/img/**/*', ['assets']);
+});
+
+
+gulp.task('default', ['sass','preproc', 'assets', 'scripts', 'sections', 'fonts', 'grid']);
